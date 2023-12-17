3a1983271eee61a8e42c3a13aa3611b4ab140d3c
jfsenechal <jfsenechal@gmail.com>
2023-12-17 19:17:37 +0100

    add parameter crud

--- src/Controller/ParameterController.php
+++ src/Controller/ParameterController.php
@@ -8,9 +8,11 @@ use App\Repository\ParameterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/parameter')]
+#[AsController]
 class ParameterController extends AbstractController
 {
     public function __construct(private readonly ParameterRepository $parameterRepository)
@@ -30,57 +32,61 @@ class ParameterController extends AbstractController
     #[Route('/new', name: 'geneacte_parameter_new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
     {
-        $actParam = new ActParams();
-        $form = $this->createForm(ActParamsType::class, $actParam);
+        $parameter = new ActParams();
+        $form = $this->createForm(ActParamsType::class, $parameter);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->parameterRepository->persist($actParam);
+            $this->parameterRepository->persist($parameter);
             $this->parameterRepository->flush();
+            $this->addFlash('success', 'Le paramètre a été ajouté');
 
-            return $this->redirectToRoute('geneacte_parameter_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('geneacte_parameter_index', []);
         }
 
-        return $this->render('parameter/new.html.twig', [
-            'act_param' => $actParam,
+        return $this->render('@ExpoActe/parameter/new.html.twig', [
+            'act_param' => $parameter,
             'form' => $form,
         ]);
     }
 
     #[Route('/{param}', name: 'geneacte_parameter_show', methods: ['GET'])]
-    public function show(ActParams $actParam): Response
+    public function show(ActParams $parameter): Response
     {
-        return $this->render('parameter/show.html.twig', [
-            'act_param' => $actParam,
+        return $this->render('@ExpoActe/parameter/show.html.twig', [
+            'act_param' => $parameter,
         ]);
     }
 
     #[Route('/{param}/edit', name: 'geneacte_parameter_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, ActParams $actParam): Response
+    public function edit(Request $request, ActParams $parameter): Response
     {
-        $form = $this->createForm(ActParamsType::class, $actParam);
+        $form = $this->createForm(ActParamsType::class, $parameter);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->parameterRepository->flush();
 
-            return $this->redirectToRoute('geneacte_parameter_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('success', 'Le paramètre a été modifié');
+
+            return $this->redirectToRoute('geneacte_parameter_index', []);
         }
 
-        return $this->render('parameter/edit.html.twig', [
-            'act_param' => $actParam,
+        return $this->render('@ExpoActe/parameter/edit.html.twig', [
+            'act_param' => $parameter,
             'form' => $form,
         ]);
     }
 
     #[Route('/{param}', name: 'geneacte_parameter_delete', methods: ['POST'])]
-    public function delete(Request $request, ActParams $actParam): Response
+    public function delete(Request $request, ActParams $parameter): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$actParam->getParam(), $request->request->get('_token'))) {
-            $this->parameterRepository->remove($actParam);
+        if ($this->isCsrfTokenValid('delete'.$parameter->getParam(), $request->request->get('_token'))) {
+            $this->parameterRepository->remove($parameter);
             $this->parameterRepository->flush();
+            $this->addFlash('success', 'Le paramètre a été supprimé');
         }
 
-        return $this->redirectToRoute('geneacte_parameter_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('geneacte_parameter_index', []);
     }
 }
