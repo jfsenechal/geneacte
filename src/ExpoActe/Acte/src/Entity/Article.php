64eb2aa5d1e1a753467ecb280b765138868fb482
jfsenechal <jfsenechal@gmail.com>
2023-12-22 19:55:59 +0100

    add article for test

---
+++ src/ExpoActe/Acte/src/Entity/Article.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ExpoActe\Acte\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ExpoActe\Acte\Repository\ArticleRepository;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
